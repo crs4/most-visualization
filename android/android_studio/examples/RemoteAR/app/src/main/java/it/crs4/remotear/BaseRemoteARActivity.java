ca924546df3ba753698247d62b00b7f650a7aa12
Mauro Del Rio <mauro@crs4.it>
2016-07-18 12:09:30 +0200

    refactoring BaseRemoteARActivity

--- android/android_studio/examples/RemoteAR/app/src/main/java/it/crs4/remotear/BaseRemoteARActivity.java
+++ android/android_studio/examples/RemoteAR/app/src/main/java/it/crs4/remotear/BaseRemoteARActivity.java
@@ -1,5 +1,6 @@
 package it.crs4.remotear;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentTransaction;
@@ -10,6 +11,8 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -17,7 +20,9 @@ import org.artoolkit.ar.base.ARToolKit;
 import org.artoolkit.ar.base.assets.AssetHelper;
 import org.artoolkit.ar.base.camera.CameraEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import it.crs4.most.streaming.IStream;
 import it.crs4.most.streaming.StreamProperties;
@@ -28,6 +33,8 @@ import it.crs4.most.streaming.enums.StreamProperty;
 import it.crs4.most.streaming.enums.StreamState;
 import it.crs4.most.streaming.enums.StreamingEvent;
 import it.crs4.most.streaming.enums.StreamingEventType;
+import it.crs4.most.visualization.IStreamFragmentCommandListener;
+import it.crs4.most.visualization.StreamInspectorFragment;
 import it.crs4.most.visualization.augmentedreality.ARFragment;
 import it.crs4.most.visualization.augmentedreality.RemoteCaptureCameraPreview;
 import it.crs4.most.visualization.augmentedreality.TouchGLSurfaceView;
@@ -35,11 +42,12 @@ import it.crs4.most.visualization.augmentedreality.renderer.PubSubARRenderer;
 
 
 public abstract class BaseRemoteARActivity extends Activity implements
+        IStreamFragmentCommandListener,
+        StreamInspectorFragment.IStreamProvider,
         ARFragment.OnCompleteListener,
         CameraEventListener {
 
     String TAG = "BaseRemoteARActivity";
-
     private IStream streamAR = null;
     private boolean streaming_ready = false;
     private Handler handler;
@@ -51,9 +59,7 @@ public abstract class BaseRemoteARActivity extends Activity implements
     private boolean mStreamPrepared = false;
     private boolean mRemotePlay = false;
     protected ARFragment streamARFragment = null;
-    private String streamURI;
     private FrameLayout streamContainer;
-    private String streamName;
     protected PubSubARRenderer renderer;
 
     public abstract String supplyStreamURI();
@@ -61,30 +67,18 @@ public abstract class BaseRemoteARActivity extends Activity implements
     public abstract String supplyStreamName();
     public abstract PubSubARRenderer supplyRenderer();
 
-
-    public String getStreamURI() {return streamURI;}
-
-    public FrameLayout  getStreamContainer() {return streamContainer;}
-
-    public String getStreamName() {return streamName;}
-
-    public PubSubARRenderer getRenderer() {return renderer;}
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         AssetHelper assetHelper = new AssetHelper(getAssets());
         assetHelper.cacheAssetFolder(this, "Data");
-        streamURI = supplyStreamURI();
-        streamContainer = supplyStreamContainer();
-        streamName = supplyStreamName();
-
     }
 
     private void setProperties() {
         StreamProperties sp = new StreamProperties();
-        sp.add(StreamProperty.URI, streamURI);
+        sp.add(StreamProperty.URI, supplyStreamURI());
         if (!streamAR.commitProperties(sp)) {
             Log.e(TAG, "failed setting stream properties");
             throw new RuntimeException("failed setting stream properties");
@@ -103,10 +97,10 @@ public abstract class BaseRemoteARActivity extends Activity implements
             // Instance the first stream
 
             HashMap<String, String> stream1_params = new HashMap<String, String>();
-            stream1_params.put("name", streamName);
+            stream1_params.put("name", supplyStreamName());
 
-            Log.d(TAG, "this.streamingUri: " + streamURI);
-            stream1_params.put("uri", streamURI);
+            Log.d(TAG, "this.streamingUri: " + supplyStreamURI());
+            stream1_params.put("uri", supplyStreamURI());
 
             this.streamAR = streamingLib.createStream(stream1_params, this.handler);
             Log.d(TAG, "createStream");
@@ -123,12 +117,11 @@ public abstract class BaseRemoteARActivity extends Activity implements
     public void playRemote() {
         mRemotePlay = true;
         if (!arFragmentAdded) {
-
-            streamARFragment = ARFragment.newInstance(streamName);
+            streamARFragment = ARFragment.newInstance(supplyStreamName());
             // add the first fragment to the first container
             FragmentTransaction fragmentTransaction = getFragmentManager()
                     .beginTransaction();
-            fragmentTransaction.add(streamContainer.getId(), streamARFragment);
+            fragmentTransaction.add(supplyStreamContainer().getId(), streamARFragment);
             fragmentTransaction.commit();
             Log.d(TAG, "fragmentTransaction.commit");
 
@@ -156,21 +149,15 @@ public abstract class BaseRemoteARActivity extends Activity implements
                             Log.d(TAG, "event.getData().streamState " + streamState);
                             Log.d(TAG, "ready to call cameraPreviewStarted");
                             IStream stream = BaseRemoteARActivity.this.streamAR;
-                            //                    int width = stream.getVideoSize().getWidth();
-                            //                    int height = stream.getVideoSize().getHeight();
-
                             //FIXME should be dynamically set
                             int width = 704;
                             int height = 576;
                             Log.d(TAG, "width " + width);
                             Log.d(TAG, "height " + height);
                             BaseRemoteARActivity.this.cameraPreviewStarted(width, height, 25, 0, false);
-
                         }
                     }
-
                 }
-
             };
             Log.d(TAG, "this.handler set");
         } else {
@@ -178,6 +165,7 @@ public abstract class BaseRemoteARActivity extends Activity implements
         }
     }
 
+//    @SuppressLint("NewApi")
     @Override
     public void cameraPreviewStarted(int width, int height, int rate, int cameraIndex, boolean cameraIsFrontFacing) {
         Log.d(TAG, "cameraPreviewStarted!");
@@ -204,8 +192,8 @@ public abstract class BaseRemoteARActivity extends Activity implements
             renderer = supplyRenderer();
         }
     }
-    private void prepareRemoteAR() {
 
+    private void prepareRemoteAR() {
         prepareAR();
         preview = (RemoteCaptureCameraPreview) findViewById(R.id.streamSurface);
         Log.i(TAG, "RemoteCaptureCameraPreview created");
@@ -216,10 +204,100 @@ public abstract class BaseRemoteARActivity extends Activity implements
 //        glView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
         glView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         glView.setZOrderMediaOverlay(true);
+    }
+    @Override
+    public void onSurfaceViewDestroyed(String streamId) {
+            this.streamAR.destroy();
+    }
+
+    @Override
+    public void onSurfaceViewCreated(String streamId, SurfaceView surfaceView) {
+        Log.d(TAG, "onSurfaceViewCreated!!!");
+        if (surfaceView != null && !mStreamPrepared) {
+            setupStreamLib();
+            prepareRemoteAR();
+            this.streamAR.prepare(surfaceView, true);
+            mStreamPrepared = true;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (this.glView != null) {
+            this.glView.onResume();
+        }
+    }
+
+    @Override
+    public void cameraPreviewStopped() {
+        ARToolKit.getInstance().cleanup();
 
     }
 
+    @Override
+    public void onFragmentCreate() {
+        Log.d(TAG, "onFragmentCreate");
+        arFragmentAdded = true;
+    }
 
+    @Override
+    public void onFragmentResume() {
+    }
 
+    @Override
+    public List<IStream> getStreams() {
+        List<IStream> streams = new ArrayList<IStream>();
+        streams.add(this.streamAR);
+        return streams;
+    }
+
+    @Override
+    public List<StreamProperty> getStreamProperties() {
+        ArrayList<StreamProperty> streamProps = new ArrayList<StreamProperty>();
+        streamProps.add(StreamProperty.NAME);
+        streamProps.add(StreamProperty.STATE);
+        return streamProps;
+    }
+
+    @Override
+    public  void onSaveInstanceState(Bundle outState){
+        outState.putBoolean("mRemotePlay", mRemotePlay);
+    }
+
+    public void cameraPreviewFrame(byte[] frame) {
+        if (this.firstUpdate) {
+            if (this.renderer.configureARScene()) {
+                Log.i(TAG, "cameraPreviewFrame(): Scene configured successfully");
+            } else {
+                Log.e(TAG, "cameraPreviewFrame(): Error configuring scene. Cannot continue.");
+                this.finish();
+            }
+
+            this.firstUpdate = false;
+        }
+
+        if (ARToolKit.getInstance().convertAndDetect(frame)) {
+            if (this.glView != null) {
+                this.glView.requestRender();
+            }
+
+//            this.onFrameProcessed();
+        } else {
+            Log.d(TAG, "no marker found, sorry");
+        }
+
+    }
+
+    @Override
+    public void onPause(String streamId) {
+        this.streamAR.pause();
+    }
+
+    @Override
+    public void onPlay(String streamId) {
+        Log.d(TAG, "stream1.play onPlay");
+        this.streamAR.play();
+    }
 
 }
--- android/android_studio/examples/RemoteAR/app/src/main/java/it/crs4/remotear/MainActivity.java
+++ android/android_studio/examples/RemoteAR/app/src/main/java/it/crs4/remotear/MainActivity.java
@@ -56,62 +56,52 @@ import org.artoolkit.ar.base.assets.AssetHelper;
 import org.artoolkit.ar.base.camera.CameraEventListener;
 
 
-public class MainActivity extends Activity implements
-//        Handler.Callback,
-        IStreamFragmentCommandListener,
-        IStreamProvider,
-        ARFragment.OnCompleteListener,
-        CameraEventListener {
+public class MainActivity extends BaseRemoteARActivity {
 
     private static String TAG = "RemoteAR";
-    private String MAIN_STREAM = "MAIN_STREAM";
 
     //ID for the menu exit option
     private final int ID_MENU_EXIT = 1;
     private boolean exitFromAppRequest = false;
 
     private Handler handler;
-    private IStream stream1 = null;
-    private boolean streaming_ready = false;
-    private boolean streamMainDestroyed = false;
 
     //    private StreamViewerFragment stream1Fragment = null;
-    private ARFragment stream1Fragment = null;
-    private String streamingUri;
 
     private EditText rtspUri;
     private Button playRemoteButton;
     private Button playLocalButton;
     private Button resetButton;
-    protected RemoteCaptureCameraPreview preview;
-    protected TouchGLSurfaceView glView;
-    protected PubSubARRenderer renderer;
-    protected FrameLayout mainLayout;
-    private boolean firstUpdate = false;
-    private boolean arFragmentAdded = false;
-    private boolean mStreamPrepared = false;
-    private boolean mRemotePlay = false;
+
+    @Override
+    public String supplyStreamURI() {
+        // FIXME
+        return "rtsp://specialista:speciali@156.148.133.11/mpeg4/media.amp";
+    }
+
+    @Override
+    public FrameLayout supplyStreamContainer() {
+        return (FrameLayout) this.findViewById(R.id.stream_container);
+    }
+
+    @Override
+    public String supplyStreamName() {
+        return "streamAR";
+    }
+
+    @Override
+    public PubSubARRenderer supplyRenderer() {
+        ZMQPublisher publisher = new ZMQPublisher();
+        Thread pubThread = new Thread(publisher);
+        pubThread.start();
+        return new PubSubARRenderer(this, publisher);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        File cacheFolder = new File(getCacheDir().getAbsolutePath() + "/Data");
-
-//        File[] files = cacheFolder.listFiles();
-//
-//        for (File file : files) {
-//
-//            if (!file.delete())
-//                throw new RuntimeException("cannot delete cached files");
-//        }
-
-        AssetHelper assetHelper = new AssetHelper(getAssets());
-        assetHelper.cacheAssetFolder(this, "Data");
-//        rtspUri = (EditText) findViewById(R.id.rtspUri);
         playRemoteButton = (Button) findViewById(R.id.play_remote);
         playLocalButton = (Button) findViewById(R.id.play_local);
         resetButton = (Button) findViewById(R.id.reset_button);
@@ -164,315 +154,6 @@ public class MainActivity extends Activity implements
     public void playLocal() {
         Intent intent = new Intent(this, LocalARActivity.class);
         startActivity(intent);
-    }
-
-    public void playRemote() {
-        mRemotePlay = true;
-        if (!arFragmentAdded) {
-
-            this.stream1Fragment = ARFragment.newInstance("stream");
-            // add the first fragment to the first container
-            FragmentTransaction fragmentTransaction = getFragmentManager()
-                    .beginTransaction();
-            fragmentTransaction.add(R.id.stream_container, this.stream1Fragment);
-            fragmentTransaction.commit();
-            Log.d(TAG, "fragmentTransaction.commit");
-
-            this.handler = new Handler(preview);
-            this.handler = new Handler(Looper.getMainLooper()) {
-                @Override
-                public void handleMessage(Message streamingMessage) {
-                    StreamingEventBundle event = (StreamingEventBundle) streamingMessage.obj;
-                    String infoMsg = "Event Type:" + event.getEventType() + " ->" + event.getEvent() + ":" + event.getInfo();
-                    Log.d(TAG, "handleMessage: Current Event:" + infoMsg);
-
-                    StreamState streamState = ((IStream) event.getData()).getState();
-                    Log.d(TAG, "event.getData().streamState " + streamState);
-                    if (event.getEventType() == StreamingEventType.STREAM_EVENT &&
-                            event.getEvent() == StreamingEvent.STREAM_STATE_CHANGED
-
-                            ) {
-                        if (streamState == StreamState.INITIALIZED) {
-                            setProperties();
-                            stream1.play();
-
-
-                        } else if (streamState == StreamState.PLAYING) {
-
-                            Log.d(TAG, "event.getData().streamState " + streamState);
-                            Log.d(TAG, "ready to call cameraPreviewStarted");
-                            IStream stream = MainActivity.this.stream1;
-                            //                    int width = stream.getVideoSize().getWidth();
-                            //                    int height = stream.getVideoSize().getHeight();
-
-                            //FIXME should be dynamically set
-                            int width = 704;
-                            int height = 576;
-                            Log.d(TAG, "width " + width);
-                            Log.d(TAG, "height " + height);
-                            MainActivity.this.cameraPreviewStarted(width, height, 25, 0, false);
-
-                        }
-                    }
-
-                }
-
-            };
-            Log.d(TAG, "this.handler set");
-        } else {
-            stream1Fragment.setStreamVisible();
-//            stream1.play();
-        }
-    }
-
-    @Override
-    public void onSurfaceViewDestroyed(String streamId) {
-        if (streamId.equals(MAIN_STREAM))
-            this.stream1.destroy();
-
-    }
-
-    @Override
-    public void onPause(String streamId) {
-        if (streamId.equals(MAIN_STREAM)) {
-            this.stream1.pause();
-        }
-    }
-
-    @Override
-    public void onPlay(String streamId) {
-        if (streamId.equals(MAIN_STREAM)) {
-            Log.d(TAG, "stream1.play onPlay");
-            this.stream1.play();
-        }
-
-    }
-
-    @Override
-    public List<IStream> getStreams() {
-        List<IStream> streams = new ArrayList<IStream>();
-        streams.add(this.stream1);
-        return streams;
-    }
-
-    @Override
-    public List<StreamProperty> getStreamProperties() {
-        ArrayList<StreamProperty> streamProps = new ArrayList<StreamProperty>();
-        streamProps.add(StreamProperty.NAME);
-        streamProps.add(StreamProperty.STATE);
-        return streamProps;
-    }
-
-    @Override
-    public void onSurfaceViewCreated(String streamId, SurfaceView surfaceView) {
-        Log.d(TAG, "onSurfaceViewCreated!!!");
-
-//        stream1Fragment.setStreamVisible();
-        if (surfaceView != null && !mStreamPrepared) {
-
-            setupStreamLib();
-            prepareRemoteAR();
-            this.stream1.prepare(surfaceView, true);
-            mStreamPrepared = true;
-        }
-    }
-
-    private void setProperties() {
-        StreamProperties sp = new StreamProperties();
-        sp.add(StreamProperty.URI, this.streamingUri);
-        if (!this.stream1.commitProperties(sp)) {
-            Log.e(TAG, "failed setting stream properties");
-            throw new RuntimeException("failed setting stream properties");
-        }
-
-    }
-
-    private void setupStreamLib() {
-        Log.d(TAG, "setupStreamLib");
-        try {
-
-            StreamingLib streamingLib = new StreamingLibBackend();
-            Log.d(TAG, "streamingLib created");
-            // First of all, initialize the library
-            streamingLib.initLib(this.getApplicationContext());
-            Log.d(TAG, "streamingLib initLib");
-            // Instance the first stream
-
-            HashMap<String, String> stream1_params = new HashMap<String, String>();
-            stream1_params.put("name", MAIN_STREAM);
-
-            this.streamingUri = "rtsp://specialista:speciali@156.148.133.11/mpeg4/media.amp";
-            Log.d(TAG, "this.streamingUri: " + this.streamingUri);
-            stream1_params.put("uri", this.streamingUri);
-
-            this.stream1 = streamingLib.createStream(stream1_params, this.handler);
-            Log.d(TAG, "createStream");
-
-
-        } catch (Exception e) {
-            streaming_ready = false;
-            Log.d(TAG, "ERROR!!!");
-            e.printStackTrace();
-        }
-
-
-        streaming_ready = true;
-//        this.stream1Fragment.setStreamVisible();
-//        this.stream1.play();
-
-
-    }
-
-    protected PubSubARRenderer supplyRenderer() {
-        ZMQPublisher publisher = new ZMQPublisher();
-        Thread pubThread = new Thread(publisher);
-        pubThread.start();
-        return new PubSubARRenderer(this, publisher);
-    }
-
-    /**
-     * Use the FrameLayout in this Activity's UI.
-     */
-
-    protected FrameLayout supplyFrameLayout() {
-        return (FrameLayout) this.findViewById(R.id.stream_container);
-    }
-
-
-    private void prepareAR() {
-        if (!ARToolKit.getInstance().initialiseNative(this.getCacheDir().getAbsolutePath())) {
-            (new AlertDialog.Builder(this)).setMessage("The native library is not loaded. The application cannot continue.").setTitle("Error").setCancelable(true).setNeutralButton(17039360, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    MainActivity.this.finish();
-                }
-            }).show();
-        } else {
-            this.mainLayout = this.supplyFrameLayout();
-            if (this.mainLayout == null) {
-                Log.e(TAG, "Error: supplyFrameLayout did not return a layout.");
-            } else {
-                this.renderer = this.supplyRenderer();
-            }
-        }
-    }
-
-    private void prepareRemoteAR() {
-
-        prepareAR();
-
-        preview = (RemoteCaptureCameraPreview) findViewById(R.id.streamSurface);
-        Log.i(TAG, "RemoteCaptureCameraPreview created");
-        preview.setCameraListener(this);
-        stream1.addFrameListener(this.preview);
-        glView = stream1Fragment.getGlView();
-        glView.setRenderer(this.renderer);
-//        glView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-        glView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        glView.setZOrderMediaOverlay(true);
-
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-//        setupStreamLib();
-
-        if (this.glView != null) {
-            this.glView.onResume();
-        }
-    }
-
-
-//    protected void onStart() {
-//        super.onStart();
-//
-//    }
-
-    @Override
-    public void cameraPreviewStarted(int width, int height, int rate, int cameraIndex, boolean cameraIsFrontFacing) {
-        Log.d(TAG, "cameraPreviewStarted!");
-        if (ARToolKit.getInstance().initialiseAR(width, height, "Data/camera_para.dat", cameraIndex, cameraIsFrontFacing)) {
-//        if (ARToolKit.getInstance().initialiseAR(width, height, "Data/camera_para_axis.dat", cameraIndex, cameraIsFrontFacing)) {
-            Log.i(TAG, "getGLView(): Camera initialised");
-        } else {
-            Log.e(TAG, "getGLView(): Error initialising camera. Cannot continue.");
-            this.finish();
-        }
-
-        Toast.makeText(this, "Camera settings: " + width + "x" + height + "@" + rate + "fps", Toast.LENGTH_SHORT).show();
-        this.firstUpdate = true;
-    }
-
-
-    public void cameraPreviewFrame(byte[] frame) {
-        if (this.firstUpdate) {
-            if (this.renderer.configureARScene()) {
-                Log.i(TAG, "cameraPreviewFrame(): Scene configured successfully");
-            } else {
-                Log.e(TAG, "cameraPreviewFrame(): Error configuring scene. Cannot continue.");
-                this.finish();
-            }
-
-            this.firstUpdate = false;
-        }
-
-        if (ARToolKit.getInstance().convertAndDetect(frame)) {
-            if (this.glView != null) {
-                this.glView.requestRender();
-            }
-
-//            this.onFrameProcessed();
-        } else {
-            Log.d(TAG, "no marker found, sorry");
-        }
-
-    }
-
-    @Override
-    public void cameraPreviewStopped() {
-        ARToolKit.getInstance().cleanup();
-
-    }
-
-    @Override
-    public void onFragmentCreate() {
-        Log.d(TAG, "onFragmentCreate");
-        arFragmentAdded = true;
-
-//        setupStreamLib();
-//        stream1Fragment.setStreamVisible();
-//        stream1.play();
-//        Log.d(TAG, "stream1.play on " + stream1.getProperty(StreamProperty.URI));
-    }
-
-    @Override
-    public void onFragmentResume() {
-//        prepareRemoteAR();
-    }
-
-    @Override
-    public void onPause(){
-        Log.d(TAG, "onPause");
-        super.onPause();
-        mStreamPrepared = false;
-    }
-
-    @Override
-    public  void onSaveInstanceState(Bundle outState){
-        outState.putBoolean("mRemotePlay", mRemotePlay);
-    }
-
-    @Override
-    public void onStop(){
-        Log.d(TAG, "onStop");
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroy(){
-        Log.d(TAG, "onDestroy");
-        super.onDestroy();
     }
 
     private void resetArrowPosition(){
