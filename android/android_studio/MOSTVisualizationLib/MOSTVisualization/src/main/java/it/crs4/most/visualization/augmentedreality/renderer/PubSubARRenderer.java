7d5c29e9145344c1d5afbc384afaf23f74eb7f21
Mauro Del Rio <mauro@crs4.it>
2016-09-27 11:43:12 +0200

    fixed bug in case no marker is associated to the mesh

--- android/android_studio/MOSTVisualizationLib/MOSTVisualization/src/main/java/it/crs4/most/visualization/augmentedreality/renderer/PubSubARRenderer.java
+++ android/android_studio/MOSTVisualizationLib/MOSTVisualization/src/main/java/it/crs4/most/visualization/augmentedreality/renderer/PubSubARRenderer.java
@@ -124,11 +124,15 @@ public class PubSubARRenderer extends ARRenderer implements Handler.Callback {
                     for (Mesh mesh : entry.getValue()) {
 
                         gl.glMatrixMode(GL10.GL_PROJECTION);
+
+                        float [] modelMatrix = new float [16];
                         if (mesh.getMarker() != null){
                             gl.glLoadMatrixf(ARToolKit.getInstance().getProjectionMatrix(), 0);
+                            modelMatrix = mesh.getMarker().getModelMatrix();
                         }
                         else{
                             gl.glLoadMatrixf(identityM, 0);
+                            Matrix.setIdentityM(modelMatrix, 0);
                         }
 
                         gl.glMatrixMode(GL10.GL_MODELVIEW);
@@ -136,7 +140,8 @@ public class PubSubARRenderer extends ARRenderer implements Handler.Callback {
 //                        gl.glLoadMatrixf(mesh.getMarker().getModelMatrix(), 0);
 //                        gl.glMultMatrixf(entry.getKey(), 0);
                         gl.glLoadMatrixf(entry.getKey(), 0);
-                        gl.glMultMatrixf(mesh.getMarker().getModelMatrix(), 0);
+
+                        gl.glMultMatrixf(modelMatrix, 0);
 
                         gl.glPushMatrix();
                         mesh.draw(gl);
