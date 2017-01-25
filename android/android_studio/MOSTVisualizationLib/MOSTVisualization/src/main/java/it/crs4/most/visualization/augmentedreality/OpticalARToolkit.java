bfd12f763a9b659072e01b3809517a0256ea513e
mdrio <mauro@crs4.it>
2017-01-25 17:01:39 +0100

    removed custom offset in eyes model matrices

--- android/android_studio/MOSTVisualizationLib/MOSTVisualization/src/main/java/it/crs4/most/visualization/augmentedreality/OpticalARToolkit.java
+++ android/android_studio/MOSTVisualizationLib/MOSTVisualization/src/main/java/it/crs4/most/visualization/augmentedreality/OpticalARToolkit.java
@@ -42,32 +42,11 @@ public class OpticalARToolkit {
     public int initialiseAR(String eyeLfilename, String eyeRfilename) {
         int ret = getOpticalMatrices(eyeLfilename, eyeRfilename, eyeLmodel, eyeLproject, eyeRmodel, eyeRproject, 1);
 
-        eyeLmodel[12] -= 70;
-        eyeRmodel[12] -= 70;
-
-        eyeLmodel[14] -= 100;
-        eyeRmodel[14] -= 100;
-//        float [] tmp = {
-//                5.715693f,
-//                0.000000f,
-//                0.000000f,
-//                0.000000f,
-//                0.000000f,
-//                10.856619f,
-//                0.000000f,
-//                0.000000f,
-//                0.000000f,
-//                0.000000f,
-//                -1.002002f,
-//                -1.000000f,
-//                0.000000f,
-//                0.000000f,
-//                -20.020020f,
-//                0.000000f
-//        };
-//        eyeRproject = tmp;
-//        eyeLproject = tmp;
-
+//        eyeLmodel[12] -= 70;
+//        eyeRmodel[12] -= 70;
+//
+//        eyeLmodel[14] -= 100;
+//        eyeRmodel[14] -= 100;
         return ret;
     }
 
